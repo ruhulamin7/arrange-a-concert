67fb25a14732b2d52bdf7e0fe8efa45102ff6ca1
ruhulaminbd93 <ruhulaminbd93@gmail.com>
2022-04-14 20:59:31 +0600

    duplicate singer restricted

--- src/components/AllSingers/AllSingers.js
+++ src/components/AllSingers/AllSingers.js
@@ -1,41 +1,46 @@
-import React, { useEffect, useState } from 'react';
-import Singer from '../Singer/Singer';
-import SelectedSingers from '../SlectedSingers/SelectedSingers';
-import './AllSingers.css'
+import React, { useEffect, useState } from "react";
+import Singer from "../Singer/Singer";
+import SelectedSingers from "../SlectedSingers/SelectedSingers";
+import "./AllSingers.css";
 
 const AllSingers = () => {
-    const [singers, setSingers] = useState([])
-    const [selecteds, setSelecteds] = useState([])
+  const [singers, setSingers] = useState([]);
+  const [selecteds, setSelecteds] = useState([]);
 
-    useEffect(() => {
-        fetch('./singers.JSON')
-            .then(res => res.json())
-            .then(data => setSingers(data))
-    }, []);
+  useEffect(() => {
+    fetch("./singers.JSON")
+      .then((res) => res.json())
+      .then((data) => setSingers(data));
+  }, []);
 
-    // add button handler
-    const handleSelectSinger = (singer) => {
-        const newSelecteds = [...selecteds, singer];
-        setSelecteds(newSelecteds);
+  // add button handler
+  const handleSelectSinger = (singer) => {
+    const duplicataSinger = selecteds.find((select) => select.id === singer.id);
+    if (duplicataSinger) {
+      alert("This singer already added");
+      return;
     }
+    const newSelecteds = [...selecteds, singer];
+    setSelecteds(newSelecteds);
+  };
 
-    return (
-        <div className="singers-container">
-            <div className="all-singers">
-                {
-                    singers.map(singer => <Singer
-                        key={singer.id}
-                        singer={singer}
-                        handleSelectSinger={handleSelectSinger}>
-                    </Singer>)
-                }
-            </div>
+  return (
+    <div className="singers-container">
+      <div className="all-singers">
+        {singers.map((singer) => (
+          <Singer
+            key={singer.id}
+            singer={singer}
+            handleSelectSinger={handleSelectSinger}
+          ></Singer>
+        ))}
+      </div>
 
-            <div className="selected-singers">
-                <SelectedSingers selecteds={selecteds}></SelectedSingers>
-            </div>
-        </div>
-    );
+      <div className="selected-singers">
+        <SelectedSingers selecteds={selecteds}></SelectedSingers>
+      </div>
+    </div>
+  );
 };
 
 export default AllSingers;
